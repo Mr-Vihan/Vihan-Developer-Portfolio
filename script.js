249a88057a93a6b4cde98e34c142ab78fac4c113
Mr-Vihan <aruvihumishra@gmail.com>
2026-02-11 18:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 <script>
   const seeMoreBtn = document.getElementById("seeMoreBtn");
+  const seeLessBtn = document.getElementById("seeLessBtn");
   const hiddenProjects = document.querySelectorAll(".project-card.hidden");
 
   seeMoreBtn.addEventListener("click", () => {
     hiddenProjects.forEach(card => {
-      card.classList.remove("hidden");
+      card.style.display = "block";
     });
 
     seeMoreBtn.style.display = "none";
+    seeLessBtn.style.display = "inline-block";
   });
 
+  seeLessBtn.addEventListener("click", () => {
+    hiddenProjects.forEach(card => {
+      card.style.display = "none";
+    });
+
+    seeLessBtn.style.display = "none";
+    seeMoreBtn.style.display = "inline-block";
+  });
 </script>
